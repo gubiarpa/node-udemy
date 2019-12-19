c43678ab593fac851308e6da12a1f2270e2e0ef5
gubiarpa <billy.arredondo@pucp.pe>
2019-12-19 15:36:20 -0500

    HBS Helpers
    Se añaden helpers, que son métodos para ser incluídos en los handlebars.

--- node/06. webserver/server.js
+++ node/06. webserver/server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 
 const hbs = require('hbs');
+require('./hbs/helpers'); // no es necesario asignarlo a una constante porque contiene ejecuciones
 
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS engine
 hbs.registerPartials(__dirname + '/views/partials'); // matriculamos un directorio de parciales
-
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('home', {
-        nombre: 'Billy',
+        nombre: 'billy arredondo palomino',
         anio: new Date().getFullYear()
     })
 });
@@ -27,7 +27,7 @@ app.get('/data', (req, res) => {
     res.send('Hello Data')
 });
 
-const port = 3005;
+const port = 3006;
 
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
