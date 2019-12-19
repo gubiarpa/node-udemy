b1eaac586395efeccbc3f8b8b05b690b2a1c4a20
gubiarpa <billy.arredondo@pucp.pe>
2019-12-19 10:39:46 -0500

    Partial Views
    Se agregó la función de vistas parciales con Express.

--- node/06. webserver/server.js
+++ node/06. webserver/server.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const app = express();
 
+const hbs = require('hbs');
+
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials'); // matriculamos un directorio de parciales
+
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Billy',
         anio: new Date().getFullYear()
-    });
-})
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    })
+});
 
 app.get('/data', (req, res) => {
     res.send('Hello Data')
-})
+});
 
-const port = 3000;
+const port = 3005;
 
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
